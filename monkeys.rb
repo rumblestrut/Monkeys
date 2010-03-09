5bb330a5a1a279746fcb473e8332ca90d54c9e98
Eric J. Gruber <eric@rumblestrut.com>
2010-03-09 12:21:50 -0600

    Added monkeys.rb

---
+++ monkeys.rb
@@ -0,0 +1,7 @@
+10.downto(1) do |x|
+	puts "#{x} little monkeys jumping on the bed."
+	puts "One fell off and bumped his head."
+	puts "Momma called the doctor and the doctor said ..."
+	puts "No more monkeys jumping on the bed!"
+	puts ""
+end 
